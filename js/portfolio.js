ad67d587e6c5a563a996d23024ca66cb44a41220
meermustan <meer.mustan786@gmail.com>
2021-01-23 17:34:48 -0800

    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,48 @@
+function aboutContainerDisplay1() {
+  container1Content=document.getElementById('container-1-content');
+  container1Content.style.display = 'flex';
+}
+function aboutContainerDisplayNone1() {
+  container1Content=document.getElementById('container-1-content');
+  container1Content.style.display = 'none';
+}
+function aboutContainerDisplay2() {
+  container2Content=document.getElementById('container-2-content');
+  container2Content.style.display = 'flex';
+}
+function aboutContainerDisplayNone2() {
+  container2Content=document.getElementById('container-2-content');
+  container2Content.style.display = 'none';
+}
+function aboutContainerDisplay3() {
+  container3Content=document.getElementById('container-3-content');
+  container3Content.style.display = 'flex';
+}
+function aboutContainerDisplayNone3() {
+  container3Content=document.getElementById('container-3-content');
+  container3Content.style.display = 'none';
+}
+function facebookContent() {
+  getFacebookContent=document.getElementById('facebook-content');
+  getFacebookContent.style.display = 'block'
+}
+function facebookContentNone() {
+  getFacebookContent=document.getElementById('facebook-content');
+  getFacebookContent.style.display = 'none'
+}
+function instaContent() {
+  getInstaContent=document.getElementById('insta-content');
+  getInstaContent.style.display = 'block'
+}
+function instaContentNone() {
+  getInstaContent=document.getElementById('insta-content');
+  getInstaContent.style.display = 'none'
+}
+function gmailContent() {
+  getGmailContent=document.getElementById('gmail-content');
+  getGmailContent.style.display = 'block'
+}
+function gmailContentNone() {
+  getGmailContent=document.getElementById('gmail-content');
+  getGmailContent.style.display = 'none'
+}
